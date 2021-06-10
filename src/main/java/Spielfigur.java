dffae2d4889dfc66454c7afdf5a6a6088347bfeb
Ludima <luedde.mahr@web.de>
2021-06-10 09:09:17 +0200

    Hinzufügen: Grundgerüst von Spielfigur implementiert.

--- src/main/java/Spielfigur.java
+++ src/main/java/Spielfigur.java
@@ -7,18 +7,12 @@
  */
 public abstract class Spielfigur
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private Farbe f;
+    
+    public Spielfigur(Farbe f) {
+        this.f = f;
     }
+    
+    public Farbe getFarbe() {return f;}
+    public void setFarbe(Farbe f) {this.f = f;}
 }
